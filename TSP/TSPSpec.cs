f2a530f098027a40ae1105649bcc4193d1f63cda
Karam217 <marcel.kollross@gmail.com>
2019-11-29 11:15:28 +0100

    Draw Canvas

---
+++ TSP/TSPSpec.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TSPSolver.TSP
+{
+    public class TSPSpec
+    {
+        public TSPSpec(string name, IReadOnlyList<int> optimalTour, IReadOnlyList<Location> cities)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("The name must not be null or whitespace", nameof(name));
+            }
+
+            Name = name;
+            OptimalTour = optimalTour ?? throw new ArgumentNullException(nameof(optimalTour));
+            Cities = cities ?? throw new ArgumentNullException(nameof(cities));
+        }
+
+        public string Name { get; }
+        public IReadOnlyList<int> OptimalTour { get; }
+        public IReadOnlyList<Location> Cities { get; }
+    }
+
+    public class Location
+    {
+        public Location(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; }
+        public double Y { get; }
+    }
+}
